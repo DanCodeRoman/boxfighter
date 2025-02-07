3bca66d1738cad03864e0f444f775ec52b890245
DanCodeRoman <0danroman@gmail.com>
2025-02-07 11:01:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,56 +1,73 @@
 // server.js
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
 
+// Enable CORS for Express routes
+app.use(cors({
+  origin: 'https://dancoderoman.github.io'  // Allow only your GitHub Pages domain
+}));
+
+// Configure Socket.IO with CORS settings
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'https://dancoderoman.github.io',
+    methods: ['GET', 'POST']
+  }
+});
+
+// A simple object to keep track of connected players
 let players = {};
 
+// Listen for new socket connections
 io.on('connection', (socket) => {
-  console.log('Player connected:', socket.id);
-  
-  // Add new player
-  players[socket.id] = { x: 0, y: 0 };
+  console.log(`Player connected: ${socket.id}`);
 
-  // Inform existing players of the new player
-  socket.broadcast.emit('newPlayer', { id: socket.id, state: players[socket.id] });
+  // Add the new player to our players object with some default state
+  players[socket.id] = {
+    id: socket.id,
+    x: 0,
+    y: 0,
+    // Add any other properties you need (e.g., score, gun, etc.)
+  };
 
-  // Send the current players list to the new player
+  // Send the current players to the new player
   socket.emit('currentPlayers', players);
 
-  // Relay movement data to everyone else
+  // Notify all other players about the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+
+  // Listen for movement events from this player
   socket.on('playerMove', (data) => {
-    players[socket.id] = data;
+    // Update the player’s data
+    players[socket.id] = { ...players[socket.id], ...data };
+
+    // Broadcast the movement to all other players
     socket.broadcast.emit('playerMove', { id: socket.id, data });
   });
 
-  // Relay shooting events
+  // Listen for shooting events from this player
   socket.on('playerShoot', (data) => {
+    // Broadcast the shooting event to all other players
     socket.broadcast.emit('playerShoot', { id: socket.id, data });
   });
 
+  // Handle player disconnect
   socket.on('disconnect', () => {
-    console.log('Player disconnected:', socket.id);
+    console.log(`Player disconnected: ${socket.id}`);
+    // Remove the player from our players object
     delete players[socket.id];
+
+    // Inform all remaining players that this player has disconnected
     socket.broadcast.emit('playerDisconnect', socket.id);
   });
 });
 
+// Optionally serve static files if needed (e.g., if you want to serve your client from here)
+// app.use(express.static('public'));
+
+const port = process.env.PORT || 3000;
 http.listen(port, () => {
-  console.log('Server listening on port', port);
+  console.log(`Server listening on port ${port}`);
 });
-
-
-socket.on("newPlayer", (data) => {
-  // Create a new opponent in the game using data.id and data.state
-});
-
-socket.on("currentPlayers", (players) => {
-  // Loop through players and add them to the game state
-});
-
-socket.on("playerDisconnect", (id) => {
-  // Remove the opponent with the given id from the game
-});
-
